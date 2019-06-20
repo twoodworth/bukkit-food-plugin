799fe79b1eb5a139ae816f141924a27975d4c783
BuildTools <unconfigured@null.spigotmc.org>
2019-06-20 16:49:36 -0400

    Initial commit

---
+++ src/main/java/me/tedwoodworth/food/Food.java
@@ -0,0 +1,15 @@
+package me.tedwoodworth.food;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Food extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("tedwashere");
+    }
+}
