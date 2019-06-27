696e6002bbea0b678dac5a42f1ae5be9361edfd6
BuildTools <unconfigured@null.spigotmc.org>
2019-06-26 23:27:01 -0400

    Disabled more ghost interactions.

--- src/main/java/me/tedwoodworth/food/PlayerEventListener.java
+++ src/main/java/me/tedwoodworth/food/PlayerEventListener.java
@@ -1,28 +1,38 @@
 package me.tedwoodworth.food;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.*;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import javax.xml.bind.Marshaller;
 
 public class PlayerEventListener implements Listener {
+
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        System.out.println("respawn");
         if (player.getGameMode() == GameMode.SURVIVAL) {
-            player.setGameMode(GameMode.SPECTATOR);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Food.plugin, () -> afterPlayerRespawn(player), 1L);
         }
+    }
+
+    public void afterPlayerRespawn(Player player) {
+        player.setGameMode(GameMode.ADVENTURE);
+
     }
 
     @EventHandler
@@ -30,8 +40,8 @@ public class PlayerEventListener implements Listener {
         Player player = event.getPlayer();
         if (event.getNewGameMode() == GameMode.ADVENTURE) {
             player.setFlySpeed(0.04f);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 255, false, true));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 1000000, 255, false, true));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 255, false, true, true));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 1000000, 255, false, true, false));
         } else {
             player.setFlySpeed(0.1f);
         }
@@ -59,6 +69,60 @@ public class PlayerEventListener implements Listener {
 
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            if (player.getGameMode() == GameMode.ADVENTURE) {
+                event.setCancelled(true);
+                player.setFireTicks(0);
+
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageEntity(EntityDamageByEntityEvent event) {
+        Entity damager = event.getDamager();
+        if (damager instanceof Player) {
+            Player player = (Player) damager;
+            if (player.getGameMode() == GameMode.ADVENTURE) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onVehicleDamage(VehicleDamageEvent event) {
+        Entity attacker = event.getAttacker();
+        if (attacker instanceof Player) {
+            Player player = (Player) attacker;
+            if (player.getGameMode() == GameMode.ADVENTURE) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onVehicleEnter(VehicleEnterEvent event) {
+        Entity entered = event.getEntered();
+        if (entered instanceof Player) {
+            Player player = (Player) entered;
+            if (player.getGameMode() == GameMode.ADVENTURE) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode() == GameMode.ADVENTURE) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onItemPickup(EntityPickupItemEvent event) {
         Entity entity = event.getEntity();
         if (entity instanceof Player) {
             Player player = (Player) entity;
